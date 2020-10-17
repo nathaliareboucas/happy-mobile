2bc4fe2e0d0aaa2249814718c72764c46aa4e1c7
Nathália Rebouças <nathreboucas5@gmail.com>
2020-10-17 00:41:44 -0300

    adicionando fonte Nunito

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
 import {Feather} from '@expo/vector-icons';
+import {useFonts} from 'expo-font';
+import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito'
 
 import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
 	return (
 		<View style={styles.container}>
       <MapView 
@@ -67,6 +77,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   calloutText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#0089a5',
     fontSize: 14
   },
@@ -87,6 +98,7 @@ const styles = StyleSheet.create({
     elevation: 5
   },
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3'
   },
   createOrphanageButton: {
